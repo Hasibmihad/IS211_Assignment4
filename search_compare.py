41355c3d580a5b0751382d65b72baf359fe9cf65
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-03 00:18:38 +0600

    functions copy done

--- search_compare.py
+++ search_compare.py
@@ -1,8 +1,72 @@
-def sequential_search():
-    return
-def ordered_sequential_search():
-    return
-def binary_search_iterative():
+def sequential_search(a_list,item):
+    pos = 0
+    found = False
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+         found = True
+        else:
+            pos = pos+1
+    return found
+ 
+
+
+def ordered_sequential_search(alist,item):
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        else:
+            if alist[pos] > item:
+                stop = True
+            else:
+                pos = pos+1
+
+    return found
+    
+
+    
+def binary_search_iterative(a_list, item):
+    first = 0
+    last = len(a_list) - 1
+    found = False
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+    return found
+   
+
+
+def binary_search_recursive(a_list,item):
+    if len(a_list) == 0:
+        return False
+    else:
+        midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+        return True
+    else:
+        if item < a_list[midpoint]:
+            return binary_search_recursive(a_list[:midpoint], item)
+        else:
+            return binary_search_recursive(a_list[midpoint + 1:], item)
     return 
-def binary_search_recursive():
-    return 
+
+
+
+
+
+
+
+def main():
+    return    
+
+
+if __name__=="__main__":
+    main()
