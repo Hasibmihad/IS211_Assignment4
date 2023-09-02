755cefc368b986c14604b0ef7919209b3700e880
Hasibmihad <mdhasibuddin12@gmail.com>
2023-09-03 00:38:46 +0600

    code copy done now my turn

---
+++ sort_compare.py
@@ -0,0 +1,44 @@
+def insertion_sort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         alist[position]=alist[position-1]
+         position = position-1
+
+     alist[position]=currentvalue
+     return alist
+   
+
+
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      #print("After increments of size",sublistcount,"The list is",alist)
+
+      sublistcount = sublistcount // 2
+    return alist
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+
+        currentvalue = alist[i]
+        position = i
+
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+
+        alist[position]=currentvalue
+
+def main():
+    return    
+
+
+if __name__=="__main__":
+    main()
